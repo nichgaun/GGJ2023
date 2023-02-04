34f2e27be460c0e44d0616388888fdfc8a4f8299
tmrupp <tmrupp@umich.edu>
2023-02-04 10:33:13 -0700

    fixed issue with first tile

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,21 +16,31 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    public Tile NextStep () {
+        Tile tile = null;
+        if (path != null && path.Count > 0) {
+            tile = (path[path.Count-1]);
+            path.RemoveAt(path.Count-1);
+        }
+        return tile;
+    }
+
     public void Seek (Tile tile) {
         path = map.FindPath(position, tile);
+        NextStep(); // throw away first step
     }
 
     void SetPosition (Tile tile) {
+        if (tile is null)
+            return;
+            
         position = tile;
         transform.position = position.transform.position + new Vector3(0f, 0f, 0.75f);
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            if (path != null && path.Count > 0) {
-                SetPosition(path[path.Count-1]);
-                path.RemoveAt(path.Count-1);
-            }
+            SetPosition(NextStep());
         }
     }
 }
