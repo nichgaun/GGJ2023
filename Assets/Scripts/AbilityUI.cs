e4939cfbed57a482dbdabe45820955da0ff9b41c
Brendon Hartbarger <riswords@hotmail.com>
2023-02-03 22:26:55 -0700

    button texture skeleton

--- Assets/Scripts/AbilityUI.cs
+++ Assets/Scripts/AbilityUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AbilityUI : MonoBehaviour
 {
@@ -10,14 +11,19 @@ public class AbilityUI : MonoBehaviour
     public Texture2D background, LOGO;
     public bool DragWindow = false;
     public bool showUI = false;
+    [SerializeField] Font font;
+    [SerializeField] Texture2D buttonTexture;
 
     private string clicked = "";
     private Rect WindowRect = new Rect((Screen.width / 2) - 400, Screen.height -120, 800, 100);
     GameManager gameManager;
+    GUIStyle ButtonStyle = new GUIStyle();
     // Start is called before the first frame update
     void Start()
     {
         gameManager = Camera.main.GetComponent<GameManager>();
+        
+        
     }
 
     private void Update() {
@@ -28,13 +34,12 @@ public class AbilityUI : MonoBehaviour
     }
     
     private void OnGUI() {
+        GUI.skin.font = font;
+        ButtonStyle = GUI.skin.button;
+        ButtonStyle.normal.background = buttonTexture;
+        
         if (showUI) {
-            if (background != null) 
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
-
-            GUI.skin = guiSkin;
-
-            WindowRect = GUI.Window(1, WindowRect, AbilityMenu, "Abilties");
+            WindowRect = GUI.Window(1, WindowRect, AbilityMenu, "Abilties", ButtonStyle);
         }
     }
 
@@ -42,7 +47,7 @@ public class AbilityUI : MonoBehaviour
         Movement movement = gameManager.GetPlayer().GetComponent<Movement>();
         GUILayout.BeginHorizontal();
         GUI.enabled = movement.moveRemaining > 0;
-        if (GUI.Button(new Rect(15,15,80,80), "Move " + movement.moveRemaining)) {
+        if (GUI.Button(new Rect(15,15,80,80), "Move " + movement.moveRemaining, ButtonStyle)) {
             //Move Function
             movement.OnClick();
         }
