b67f0072bb2f58b231a1e09ba50691b3cada0799
tmrupp <tmrupp@umich.edu>
2023-02-03 19:19:37 -0700

    neighbors

--- Assets/Scripts/HexMap.cs
+++ Assets/Scripts/HexMap.cs
@@ -45,18 +45,44 @@ public class HexMap : MonoBehaviour {
         return new Vector2Int (q, r);
     }
 
+    Tile FromAxial (Vector2Int hex) {
+        var col = hex.x + (hex.y - (hex.y&1)) / 2;
+        var row = hex.y;
+        return GetTile(new Vector2Int(col, row));
+
+    }
+
     public int GetDistance (Tile a, Tile b) {
         var ac = ToAxial(a);
         var bc = ToAxial(b);
         return AxialDistance(ac, bc);
     }
 
-    public Tile GetTile (Vector2Int coordinate) {
-        return map[coordinate.x][coordinate.y].GetComponent<Tile>();
+    public Tile GetTile (Vector2Int hex) {
+        if (hex.x >= width || hex.x < 0 || hex.y >= height || hex.y < 0)
+            return null;
+
+        return map[hex.x][hex.y].GetComponent<Tile>();
     }
+
+    List<Vector2Int> AxialDirections = new List<Vector2Int> {
+        new Vector2Int (1, 0),
+        new Vector2Int (1, -1),
+        new Vector2Int (0, -1),
+        new Vector2Int (-1, 0),
+        new Vector2Int (-1, 1),
+        new Vector2Int (0, 1),
+    };
 
     public List<Tile> GetNeighbors (Tile tile) {
         List<Tile> tiles = new List<Tile>();
+
+        var axial = ToAxial(tile);
+        foreach (var direction in AxialDirections) {
+            var neighbor = FromAxial(axial + direction);
+            if (tile != null)
+                tiles.Add(neighbor);
+        }
 
         return tiles;
     }
