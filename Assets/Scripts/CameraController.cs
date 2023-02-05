afd7a5be4898b51fd85af0d0e94dba1377b459e9
tmrupp <tmrupp@umich.edu>
2023-02-05 11:53:05 -0700

    can move around camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    float panSpeed = 10f;
+    float panSpeed = 40f, zoomSpeed = 500f;
     
     void Update () {
         transform.position += (Input.GetAxisRaw("Vertical")*Vector3.forward + Input.GetAxisRaw("Horizontal")*Vector3.right)*panSpeed*Time.deltaTime;
+        transform.position += transform.forward * Input.mouseScrollDelta.y * Time.deltaTime*zoomSpeed;
     }
 }
