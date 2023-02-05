32ed1ea327088db0be31c52f3edf8526ff9c924a
Brendon Hartbarger <riswords@hotmail.com>
2023-02-05 01:53:00 -0700

    level builder player generation

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -8,15 +8,21 @@ public class LevelBuilder : MonoBehaviour
     [SerializeField] List<Vector2> rangedBaddieCoords;
     [SerializeField] List<Vector2> meleeMonsterCoords;
 
-    [SerializeField] Vector2 playerCoords;
+    [SerializeField] Vector2Int playerCoords;
 
     [SerializeField] List<Vector2> impassableTerrainCoords;
 
-    public void Start() {
 
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] HexMap hexMap;
+
+
+    public void Start() {
+        GeneratePlayer(playerCoords);
     }
 
-    public void GeneratePlayer(Vector2 coordss) {
-
+    public void GeneratePlayer(Vector2Int coords) {
+        GameObject player = Instantiate(playerPrefab);
+        player.GetComponent<Player>().SetPosition(hexMap.GetTile(coords));
     }
 }
