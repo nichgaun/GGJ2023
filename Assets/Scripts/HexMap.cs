9c8f8aed22ccd6d2104df29aff6981acf185896e
tmrupp <tmrupp@umich.edu>
2023-02-04 18:05:59 -0700

    no more red lines

--- Assets/Scripts/HexMap.cs
+++ Assets/Scripts/HexMap.cs
@@ -167,12 +167,12 @@ public class HexMap : MonoBehaviour {
         if (finders.Count >= 2) {
             int c = finders.Count;
             // path = FindPath(finders[c-2], finders[c-1]);
-            path = LineDraw(finders[c-2], finders[c-1]);
-            Debug.Log("path.Count=" + path.Count);
-            foreach (Tile current in path) {
-                Debug.Log("current="+current);
-                current.ChangeColor(Color.red);
-            }
+            // path = LineDraw(finders[c-2], finders[c-1]);
+            // Debug.Log("path.Count=" + path.Count);
+            // foreach (Tile current in path) {
+            //     Debug.Log("current="+current);
+            //     current.ChangeColor(Color.red);
+            // }
         }
     }
 
